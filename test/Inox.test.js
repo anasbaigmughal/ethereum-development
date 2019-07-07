c530d6940e1d4613cbea40b0e4191e94bbb5794d
AnasBaigMughal <anasbaigmughal@gmail.com>
2019-07-07 12:57:20 +0500

    modules installation

--- test/Inox.test.js
+++ test/Inox.test.js
@@ -1,3 +1,4 @@
 const assert = require('assert'); //asert standard library build on nodejs runtime, assert is used to make assertions for test that some value is equal to another value
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); //capital case because we will work with constructor moduel so this iis conventionn for that
+//web3 is used for interaction between javascrfipt application and ethereum network
